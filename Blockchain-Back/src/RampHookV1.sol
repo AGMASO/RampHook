c025cd49c793fdd53c8c0f6dfd37b54f43405423
Alejandro <95312462+AGMASO@users.noreply.github.com>
2025-06-24 22:35:24 +0200

    docs: clean code

--- Blockchain-Back/src/RampHookV1.sol
+++ Blockchain-Back/src/RampHookV1.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {console2} from "forge-std/Test.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
 import {IERC20Minimal} from "v4-core/interfaces/external/IERC20Minimal.sol";
-
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
@@ -15,9 +14,10 @@ import {SwapParams} from "v4-core/types/PoolOperation.sol";
 import {StateLibrary} from "v4-periphery/lib/v4-core/src/libraries/StateLibrary.sol";
 import {TickMath} from "v4-periphery/lib/v4-core/src/libraries/TickMath.sol";
 import {Currency} from "v4-core/types/Currency.sol";
-import {IRampHookV1} from "./interfaces/IRampHookV1.sol";
 import {CurrencySettler} from "@uniswap/v4-core/test/utils/CurrencySettler.sol";
 import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
+
+import {IRampHookV1} from "./interfaces/IRampHookV1.sol";
 
 contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
     using CurrencySettler for Currency;
@@ -25,7 +25,7 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
     using LPFeeLibrary for uint24;
 
     address private s_vault;
-
+    /// @notice mapping to store pending on-ramp orders
     mapping(PoolId poolId => mapping(bool zeroForOne => OnRampOrder[]))
         public pendingOrders;
 
@@ -36,14 +36,14 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
         require(_vault != address(0), "Vault address cannot be zero");
         s_vault = _vault;
     }
-
+    /// @notice modifier to restrict actions to the vault only
     modifier onlyVault() {
         if (msg.sender != address(s_vault)) {
             revert RampHook_OnlyVaultCanCreateOnRampOrder();
         }
         _;
     }
-
+    /// @inheritdoc BaseHook
     function getHookPermissions()
         public
         pure
@@ -68,7 +68,7 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
                 afterRemoveLiquidityReturnDelta: false
             });
     }
-
+    /// @inheritdoc BaseHook
     function _beforeInitialize(
         address,
         PoolKey calldata key,
@@ -102,7 +102,7 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
         PoolKey memory key = callbackData.key;
 
         OnRampOrder memory newOrder = OnRampOrder({
-            inputAmount: -swapParams.amountSpecified, //! lo estamos guardadon en positivo
+            inputAmount: uint256(-swapParams.amountSpecified), //! storing as a positive value
             receiver: receiver,
             fulfilled: false
         });
@@ -110,7 +110,7 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
         pendingOrders[key.toId()][swapParams.zeroForOne].push(newOrder); // esto esta en negativo
         if (swapParams.zeroForOne) {
             // function settle(Currency currency, IPoolManager manager, address payer, uint256 amount, bool burn) internal {
-            //!aqui estamos enviano Token0 al PM en el caso de que sea zeroForOne
+            //! Here we are sending Token0 to the Pool Manager in the case of zeroForOne
             key.currency0.settle(
                 poolManager,
                 sender,
@@ -118,7 +118,7 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
                 false
             );
             // function take(Currency currency, IPoolManager manager, address recipient, uint256 amount, bool claims) internal {
-            //! Estamos dando Claims TOkens por los tokens enviados al Hook
+            //! We are issuing Claim Tokens for the tokens sent to the Hook
             key.currency0.take(
                 poolManager,
                 address(this),
@@ -150,7 +150,7 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
         require(_vault != address(0), "Vault address cannot be zero");
         s_vault = _vault;
     }
-
+    /// @inheritdoc BaseHook
     function _beforeSwap(
         address sender,
         PoolKey calldata key,
@@ -160,10 +160,9 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
         //Obtain the pool price
         (, int24 currentTick, , ) = poolManager.getSlot0(key.toId());
         uint256 price = _tickToPrice(currentTick);
-        console2.log("Esto es el precio de la pool:", price); //! esto es un precio Q64.96, lo convertimos a Q18
+        console2.log("This is the pool price:", price);
         int256 expectedOut = _getExpectedOutput(-params.amountSpecified, price);
-        console2.log("Esto es el expectedOutput del swap:", expectedOut);
-        // bool oppositeDirection = !params.zeroForOne; //! vamos a buscar solo los orders opuestos al swap entrante
+        console2.log("This is the expected output of the swap:", expectedOut);
 
         return _matchOrders(key, params, expectedOut);
     }
@@ -186,14 +185,14 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
         (Currency userSellToken, Currency userBuyToken) = zeroForOne
             ? (key.currency0, key.currency1)
             : (key.currency1, key.currency0);
-        //! se acunan claimsTokens0 al Hook
+        //! claimTokens0 are minted to the Hook
         userSellToken.take(
             poolManager,
             address(this),
             uint256(uint128(inputAmount)),
             true
         );
-        //!Quema los claimtoknes1 de Hook.
+        //!Burns the claimTokens1 from the Hook.
         userBuyToken.settle(
             poolManager,
             address(this),
@@ -239,9 +238,17 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
             !params.zeroForOne
         ];
 
-        int256 remOut = expectedOut; // output que falta entregar
-        int256 matched0; // token0 ya liquidado por Hook
-        int256 matched1; // token1 ya liquidado por Hook
+        /**
+         * @dev Tracks the remaining output amount that needs to be delivered.
+         * @param remOut The amount of output tokens still to be delivered.
+         *
+         * @dev Tracks the amount of token0 and token1 already settled by the Hook.
+         * @param matched0 The amount of token0 that has been liquidated by the Hook.
+         * @param matched1 The amount of token1 that has been liquidated by the Hook.
+         */
+        int256 remOut = expectedOut;
+        int256 matched0;
+        int256 matched1;
 
         for (uint256 i; i < orders.length && remOut > 0; ++i) {
             OnRampOrder storage order = orders[i];
@@ -251,7 +258,7 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
                 ? order.inputAmount
                 : remOut;
 
-            // proporción de token0 que hace falta para cubrir `takeOut`
+            // proportion of token0 needed to cover `takeOut`
             int256 takeIn = (takeOut * -params.amountSpecified) / expectedOut;
 
             _settleAndTake(
@@ -262,14 +269,13 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
                 order.receiver
             );
 
-            matched0 += takeIn; // acumulamos para el delta
+            matched0 += takeIn; // accumulate for the delta
             matched1 -= takeOut;
 
             remOut -= takeOut;
             if (order.inputAmount <= takeOut) order.fulfilled = true;
         }
 
-        // Δ que le indica al núcleo lo que YA liquidó el Hook
         BeforeSwapDelta deltaHook = matched0 == 0
             ? BeforeSwapDeltaLibrary.ZERO_DELTA
             : toBeforeSwapDelta(int128(matched0), int128(matched1));
@@ -278,7 +284,13 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
         uint24 feeWithFlag = fee | LPFeeLibrary.OVERRIDE_FEE_FLAG;
         return (this.beforeSwap.selector, deltaHook, feeWithFlag);
     }
-
+    /// @notice Calculates the expected output amount based on the input amount and price
+    function _getExpectedOutput(
+        int256 _amountSpecified,
+        uint256 _price
+    ) private pure returns (int256 expectedOutput) {
+        expectedOutput = (_amountSpecified * int256(_price)) / 1e18;
+    }
     /// @inheritdoc IRampHookV1
     function getPendingOrders(
         PoolId poolId,
@@ -289,21 +301,9 @@ contract RampHookV1 is IRampHookV1, BaseHook, Ownable {
 
     /// @inheritdoc IRampHookV1
     function _tickToPrice(int24 currentTick) public pure returns (uint256) {
-        // uint160 sqrtPriceX96 = TickMath.getSqrtPriceAtTick(currentTick);
-        // // Convertir sqrtPriceX96 a un precio decimal (por ejemplo, Q64.96 a Q18)
-        // return (uint256(sqrtPriceX96) * uint256(sqrtPriceX96)) / (1 << 192);
-
         uint160 sqrtPriceX96 = TickMath.getSqrtPriceAtTick(currentTick);
         uint256 num = uint256(sqrtPriceX96) * uint256(sqrtPriceX96);
-        // ← Q64.96 → Q18: multiplica por 1e18 ANTES del shift
-        return (num * 1e18) >> 192;
-    }
 
-    function _getExpectedOutput(
-        int256 _amountSpecified,
-        uint256 _price
-    ) private pure returns (int256 expectedOutput) {
-        // expectedOutput = (_amountSpecified * int256(_price));
-        expectedOutput = (_amountSpecified * int256(_price)) / 1e18;
+        return (num * 1e18) >> 192;
     }
 }
